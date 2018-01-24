52892a2fc714fcb2416b4efc43edf25f2b40acc9
Diego Cintra <diego_2337@hotmail.com>
2018-01-24 12:38:24 -0200

    Now adding "download.json" button functionality.

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1133,6 +1133,20 @@ $('#saveImgButton').on('click', function (){
   // document.getElementById('saveImg').click();
 });
 
+$('#saveFileButton').on('click', function(){
+  $.ajax({
+    url: '/switch',
+    type: 'POST',
+    success: function(html){
+      var dataStr = "data:text/javascript;charset=utf-8," + encodeURIComponent(JSON.stringify(html));
+      var dlAnchorElem = document.getElementById('downloadAnchorElem');
+      dlAnchorElem.setAttribute("href", dataStr);
+      dlAnchorElem.setAttribute("download", "graph.json");
+      dlAnchorElem.click();
+    }
+  });
+});
+
 /**
   * File to watch for changes in slider used for multilevel paradigm.
   * Author: Diego Cintra
--- public/javascripts/saves.js
+++ public/javascripts/saves.js
@@ -11,3 +11,17 @@ $('#saveImgButton').on('click', function (){
   document.getElementById('saveImg').href = dataURL;
   // document.getElementById('saveImg').click();
 });
+
+$('#saveFileButton').on('click', function(){
+  $.ajax({
+    url: '/switch',
+    type: 'POST',
+    success: function(html){
+      var dataStr = "data:text/javascript;charset=utf-8," + encodeURIComponent(JSON.stringify(html));
+      var dlAnchorElem = document.getElementById('downloadAnchorElem');
+      dlAnchorElem.setAttribute("href", dataStr);
+      dlAnchorElem.setAttribute("download", "graph.json");
+      dlAnchorElem.click();
+    }
+  });
+});
