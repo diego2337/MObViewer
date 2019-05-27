60caa0d045113b51d6f484adca68642f8e48ba6c
Diego Cintra <diego_2337@hotmail.com>
2019-05-27 15:46:18 -0300

    Adding new dependencies to 'dependencies.sh' script.

--- dependencies.sh
+++ dependencies.sh
@@ -8,11 +8,15 @@ sudo apt-get install python-pypdf2
 sudo apt-get install python-pip
 sudo apt-get install -y libigraph0-dev
 sudo apt-get install python-all-dev
-sudo pip install python-igraph
-sudo pip install scipy
+sudo apt-get install npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+nvm install node
+sudo npm install grunt --g
+sudo pip3 install python-igraph
+sudo pip3 install scipy
 sudo apt-get install python-sklearn
 sudo apt-get install pip
-sudo pip-install pyyaml
-sudo pip install Pillow
-sudo pip install networkx
-sudo pip install sharedmem
+sudo pip3-install pyyaml
+sudo pip3 install Pillow
+sudo pip3 install networkx
+sudo pip3 install sharedmem
