d8631e00f41ec90d9aecc009708b6e3a3b11d224
Diego Cintra <diego_2337@hotmail.com>
2019-05-27 14:31:35 -0300

    Adding 'dependencies' script for 'MObViewer'.

---
+++ dependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Install dependencies for 'MObViewer' project.
+# Author: Diego Cintra
+# Date: 27 May 2019
+
+sudo apt-get install python-setuptools
+sudo apt-get install python-pypdf2
+sudo apt-get install python-pip
+sudo apt-get install -y libigraph0-dev
+sudo apt-get install python-all-dev
+sudo pip install python-igraph
+sudo pip install scipy
+sudo apt-get install python-sklearn
+sudo apt-get install pip
+sudo pip-install pyyaml
+sudo pip install Pillow
+sudo pip install networkx
+sudo pip install sharedmem
